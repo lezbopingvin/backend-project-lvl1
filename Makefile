27e8c534a747394c62c5c8061e042a8e852caada
lezbopingvin <lezbopingvin@gmail.com>
2021-02-17 18:19:37 +0300

    #4

--- Makefile
+++ Makefile
@@ -6,5 +6,8 @@ brain-games:
 
 publish:
 	npm publish --dry-run
+
+lint:
+	npx eslint .
 	
 .PHONY: test
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const greet = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}`);
-}
-
+export default () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}`);
+};
